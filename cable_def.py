fcc21e4f484f2dd820c6c4afaad48a042bc8396a
Abigayle Moser <85366919+am0ser@users.noreply.github.com>
2021-11-01 15:06:28 -0600

    begin to define cable classes

--- cable_def.py
+++ cable_def.py
@@ -1,1 +1,29 @@
+#define cables
 
+
+
+import numpy
+import os
+import re
+import pandas as pd
+
+
+class ArrayCable:
+    #base cable class
+    def __init__(self, arraycable_specs):
+        ##creates instance of array cable
+            #arraycable_specs: dict , dictionary containing array cable system specifications
+        self.extract_arraycable_specs(arraycable_specs)
+    def extract_arraycable_specs(self, arraycable_specs):
+        ##extracts and defines array cable specifications
+
+        #operational parameters
+        self.rated_voltage = arraycable_specs.get("rated_voltage", None)
+        self.xsection = arraycable_specs.get("xsection", None)
+        slef.powerperkgm = arraycable_specs.get("powerperkgm", None)
+        self.material = arraycable_specs.get("material", None)
+        self.ac_resistance = arraycable_specs.get("ac_resistance", None)
+        self.capacitance = arraycable_specs.get("capacitance", None)
+        self.inductance = arraycable_specs.get("inductance", None)
+        self.linear_density = arraycable_specs.get("linear_density", None)
+        self.current_capacity = arraycable_specs.get("current_capacity", None)
