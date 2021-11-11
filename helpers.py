f04b3319eb86c9517e984a1d870fc7b61005af26
Abigayle Moser <85366919+am0ser@users.noreply.github.com>
2021-11-11 13:17:18 -0700

    add helpers indicies, define cables

---
+++ helpers.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+critmat_LH indicies = {
+    'vltg_kV': 1,
+    'insltn_630mm': 2,
+    'insltn_d_630mm': 3,
+    'Pb_630mm': 4,
+    'outer_d_630mm': 5,
+    'mass_630kg_m': 6,
+    'insltn_800mm': 11,
+    'insltn_d_800mm': 12,
+    'Pb_800mm': 13,
+    'outer_d_800mm': 14,
+    'mass_800kg_m': 15,
+    'insltn_1000mm': 20,
+    'insltn_d_1000mm': 21,
+    'Pb_1000mm': 22,
+    'outer_d_1000mm': 23,
+    'mass_1000kg_m': 24,
+    'insltn_1200mm': 29,
+    'insltn_d_1200mm': 30,
+    'Pb_1200mm': 31,
+    'outer_d_1200mm': 32,
+    'mass_1200kg_m': 33,
+
+}
